355ae9cce6badabeeb3cbcf9a892925cfa2293c6
Rohan Kale <rkale843@student.fuhsd.org>
2019-04-10 22:40:44 -0700

    made a check for if player is close to the wumpus

--- src/Wumpus.java
+++ src/Wumpus.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Wumpus extends Creature{
     Graph.Node playerRoom = null;
     public Wumpus(String name, Graph.Node currentRoom){
@@ -16,8 +18,26 @@ public class Wumpus extends Creature{
     }
 
     private boolean isWithin2StepsOfPlayer() {
-//        if ()
-        return true;
+        ArrayList<String> neighborsForCurrentRoom = new ArrayList<>(currentRoom.getNeighbors().keySet());
+        ArrayList<String> neighborsForPlayerRoom = new ArrayList<>(playerRoom.getNeighbors().keySet());
+        if(neighborsForCurrentRoom.size()>neighborsForPlayerRoom.size()) {
+            for (int i = 0; i < neighborsForCurrentRoom.size(); i++) {
+                String nodeName = neighborsForCurrentRoom.get(i);
+                if (neighborsForPlayerRoom.contains(nodeName)){
+                    return true;
+                }
+            }
+        }
+        else{
+            for (int i = 0; i < neighborsForPlayerRoom.size(); i++) {
+                 String nodeName = neighborsForPlayerRoom.get(i);
+                if (neighborsForCurrentRoom.contains(nodeName)){
+                    return true;
+                }
+
+            }
+        }
+        return false;
     }
 
     public void getPlayerRoom(Player p){
